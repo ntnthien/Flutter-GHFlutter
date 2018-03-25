257529abc91a7d4d7132e31aaffd274b6b440d23
Do Nguyen <daniel@zincer.com>
2018-03-25 23:57:46 +0700

    add network call + member model

--- lib/main.dart
+++ lib/main.dart
@@ -23,13 +23,64 @@ class GHFlutter extends StatefulWidget {
 }
 
 class GHFlutterState extends State<GHFlutter> {
+  var _members = <Member>[];
+
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(Strings.appTitle),
       ),
-      body: new Text(Strings.appTitle),
+      body: new ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemCount: _members.length,
+          itemBuilder: (BuildContext context, int position) {
+            if (position.isOdd) return new Divider();
+
+            final index = position ~/ 2;
+
+            return _buildRow(index);
+          }),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadData();
+  }
+
+  Widget _buildRow(int i) {
+    return new Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: new ListTile(
+            title: new Text("${_members[i].login}", style: _biggerFont)));
+  }
+
+  _loadData() async {
+    String dataURL = "https://api.github.com/orgs/raywenderlich/members";
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      final membersJSON = JSON.decode(response.body);
+
+      for (var memberJSON in membersJSON) {
+        final member = new Member(memberJSON["login"]);
+        _members.add(member);
+      }
+    });
+  }
+}
+
+class Member {
+  final String login;
+
+  Member(this.login) {
+    if (login == null) {
+      throw new ArgumentError("login of Member cannot be null. "
+          "Received: '$login'");
+    }
   }
 }
