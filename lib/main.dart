ce95e1080e84a1934beba59a252dcdfe6dec1c1e
Do Nguyen <daniel@zincer.com>
2018-03-26 00:04:32 +0700

    clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
+import 'ghflutter.dart';
 import 'strings.dart';
 
 void main() => runApp(new GHFlutterApp());
@@ -14,83 +12,5 @@ class GHFlutterApp extends StatelessWidget {
       title: Strings.appTitle,
       home: new GHFlutter(),
     );
-  }
-}
-
-class GHFlutter extends StatefulWidget {
-  @override
-  createState() => new GHFlutterState();
-}
-
-class GHFlutterState extends State<GHFlutter> {
-  var _members = <Member>[];
-
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(Strings.appTitle),
-      ),
-      body: new ListView.builder(
-          padding: const EdgeInsets.all(16.0),
-          itemCount: _members.length,
-          itemBuilder: (BuildContext context, int position) {
-            if (position.isOdd) return new Divider();
-
-            final index = position ~/ 2;
-
-            return _buildRow(index);
-          }),
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    _loadData();
-  }
-
-  Widget _buildRow(int i) {
-    return new Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: new ListTile(
-          title: new Text("${_members[i].login}", style: _biggerFont),
-          leading: new CircleAvatar(
-              backgroundColor: Colors.green,
-              backgroundImage: new NetworkImage(_members[i].avatarUrl)),
-        ));
-  }
-
-  _loadData() async {
-    String dataURL = "https://api.github.com/orgs/raywenderlich/members";
-    http.Response response = await http.get(dataURL);
-    setState(() {
-      final membersJSON = JSON.decode(response.body);
-
-      for (var memberJSON in membersJSON) {
-        final member =
-            new Member(memberJSON["login"], memberJSON["avatar_url"]);
-        _members.add(member);
-      }
-    });
-  }
-}
-
-class Member {
-  final String login;
-  final String avatarUrl;
-
-  Member(this.login, this.avatarUrl) {
-    if (login == null) {
-      throw new ArgumentError("login of Member cannot be null. "
-          "Received: '$login'");
-    }
-    if (avatarUrl == null) {
-      throw new ArgumentError("avatarUrl of Member cannot be null. "
-          "Received: '$avatarUrl'");
-    }
   }
 }
